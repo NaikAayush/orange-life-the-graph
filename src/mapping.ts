2e4b5769bdbf408ee66e8f03031b2e550017b17c
Samyak S Sarnayak <samyak201@gmail.com>
2021-08-06 19:28:26 +0530

    handle request, grant and revoke access events
     - also updated contract address and stuff
     - thanks to Simon from the Graph discord for helping fix an issue
     - also renamed gravatar to medRecord
     - next commit will have updated contract ABI

--- src/mapping.ts
+++ src/mapping.ts
@@ -8,19 +8,41 @@ import {
 } from "../generated/OrangeLife/OrangeLife";
 import { MedicalRecord } from "../generated/schema";
 
-export function handleGrantedAccess(event: GrantedAccess): void {}
-
 export function handleNewMedicalRecord(event: NewMedicalRecord): void {
-  let gravatar = new MedicalRecord(event.transaction.from.toHex());
-  gravatar.owner = event.params.owner.toHexString();
-  gravatar.idx = event.params.idx;
-  gravatar.docCID = event.params.docCID;
-  gravatar.verifyingKey = event.params.verifyingKey;
-  gravatar.publicKey = event.params.publicKey;
-  gravatar.nonce = event.params.nonce;
-  gravatar.save();
+  let medRecord = new MedicalRecord(event.params.docCID);
+  medRecord.owner = event.params.owner.toHexString();
+  medRecord.idx = event.params.idx;
+  medRecord.docCID = event.params.docCID;
+  medRecord.verifyingKey = event.params.verifyingKey;
+  medRecord.publicKey = event.params.publicKey;
+  medRecord.nonce = event.params.nonce;
+  medRecord.hasAccess = [medRecord.owner];
+  medRecord.accessRequested = [];
+  medRecord.save();
 }
 
-export function handleRequestedAccess(event: RequestedAccess): void {}
+export function handleRequestedAccess(event: RequestedAccess): void {
+  let medRecord = MedicalRecord.load(event.params.docCID);
+  let accessRequested = medRecord.accessRequested;
+  accessRequested.push(event.params.requestor.toHexString());
+  medRecord.accessRequested = accessRequested;
+  medRecord.save();
+}
 
-export function handleRevokedAccess(event: RevokedAccess): void {}
+export function handleGrantedAccess(event: GrantedAccess): void {
+  let medRecord = MedicalRecord.load(event.params.docCID);
+  let hasAccess = medRecord.hasAccess;
+  // owner is actually addrToGrant
+  hasAccess.push(event.params.owner.toHexString());
+  medRecord.hasAccess = hasAccess;
+  medRecord.save();
+}
+
+export function handleRevokedAccess(event: RevokedAccess): void {
+  let medRecord = MedicalRecord.load(event.params.docCID);
+  // owner is actually addrToRevoke
+  medRecord.hasAccess = medRecord.hasAccess.filter((addr) => {
+    return addr != event.params.owner.toHexString();
+  });
+  medRecord.save();
+}
