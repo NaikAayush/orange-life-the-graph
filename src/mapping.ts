98fd7571a81bd5446ac044450f601d3970f2d30e
NaikAayush <ayush@pesu.pes.edu>
2021-08-06 11:36:10 +0530

    new

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,60 +1,21 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt } from "@graphprotocol/graph-ts";
 import {
   OrangeLife,
   GrantedAccess,
   NewMedicalRecord,
   RequestedAccess,
-  RevokedAccess
-} from "../generated/OrangeLife/OrangeLife"
-import { ExampleEntity } from "../generated/schema"
+  RevokedAccess,
+} from "../generated/OrangeLife/OrangeLife";
+import { MedicalRecord } from "../generated/schema";
 
-export function handleGrantedAccess(event: GrantedAccess): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+export function handleGrantedAccess(event: GrantedAccess): void {}
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.requestor = event.params.requestor
-  entity.owner = event.params.owner
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.isTrustedForwarder(...)
-  // - contract.trustedForwarder(...)
-  // - contract.getMedicalRecords(...)
-  // - contract.getMedicalRecord(...)
-  // - contract.versionRecipient(...)
+export function handleNewMedicalRecord(event: NewMedicalRecord): void {
+  let gravatar = new MedicalRecord(event.transaction.from.toHex());
+  gravatar.owner = event.params.owner;
+  gravatar.docCID = event.params.idx;
+  gravatar.save();
 }
-
-export function handleNewMedicalRecord(event: NewMedicalRecord): void {}
 
 export function handleRequestedAccess(event: RequestedAccess): void {}
 
